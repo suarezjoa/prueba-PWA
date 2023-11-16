c221afa45f8bc618f332799db2a0c8af56fb5a36
Akoo <gnoattosuarezjoaquin@gmail.com>
2023-11-16 02:37:13 -0300

    avacnces

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import router from "./router";
 import "./index.css";
 import { AuthProvider } from "./context/AuthProvider";
 import { MesasProvider } from "./context/MesasProvider";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
+  <GoogleOAuthProvider clientId="1051608997183-prdq5s4ntcqbtj3ad83c0no4nv4qa599.apps.googleusercontent.com">
     <AuthProvider>
       <MesasProvider>
         <RouterProvider router={router} />
       </MesasProvider>
     </AuthProvider>
-  
+  </GoogleOAuthProvider>
 );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
 import useAuth from "../hooks/useAuth";
 import Logo from "../assets/logo.png";
-
+import { GoogleLogin } from '@react-oauth/google';
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -106,33 +106,14 @@ const Login = () => {
 
             <div className="flex flex-col justify-center items-center   mt-20 gap-2">
               
-              {/* <button className="   font-semibold flex justify-center items-center gap-2 border-black rounded-xl p-3 bg-gray-200 ">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="20"
-                  viewBox="0 0 256 262"
-                >
-                  <path
-                    fill="#4285F4"
-                    d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
-                  />
-                  <path
-                    fill="#34A853"
-                    d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
-                  />
-                  <path
-                    fill="#FBBC05"
-                    d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"
-                  />
-                  <path
-                    fill="#EB4335"
-                    d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
-                  />
-                </svg>
-                Google
-              </button> */}
-
-              
+            <GoogleLogin
+                onSuccess={credentialResponse => {
+                  console.log(credentialResponse);
+                }}
+                onError={() => {
+                  console.log('Login Failed');
+                }}
+            />
             </div>
           </div>
         </div>
